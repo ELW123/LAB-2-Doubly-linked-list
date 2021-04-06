fbd1a4bb0c37dd7612687dd8c6db50e3ba64d29c
mgolw001 <56405905+mgolw001@users.noreply.github.com>
2021-04-06 10:37:42 -0700

    Create IntList.cpp

---
+++ IntList.cpp
@@ -0,0 +1,39 @@
+#include "IntList.h"
+#include <iostream>
+
+using namespace std;
+
+IntList::IntList () {
+	dummyhead = 0;
+	dummytail = 0;
+}
+
+IntList::~IntList() {
+	
+}
+
+void IntList::push_front(int value){
+	
+}
+
+void IntList::pop_front(){
+	
+}
+
+void IntList::push_back(int value){
+	
+}
+
+void IntList::pop_back(){
+	
+}
+
+bool IntList::empty() const{
+}
+
+ostream IntList:: & operator<<(ostream &out, const IntList &rhs){ // i don't know if this is defined properly
+	
+}
+
+void IntList::printReverse() const{
+}
